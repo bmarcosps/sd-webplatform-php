8c3ff22201fd596f9f4be4b1e7bf0b1e67cc120e
Vini <viniciusalves@ice.ufjf.br>
2019-11-26 22:24:20 -0300

    Algumas mudancas em listas salas

--- public_html/rooms.php
+++ public_html/rooms.php
@@ -2,9 +2,51 @@
 include('includes/config.php');
 $pageTitle = "Salas";
 
-if(!isset($_SESSION['user']))
+if(!isset($_SESSION['userIntegra']))
 {
     header('location:login.php');
+} else {
+   /* $data = array(
+        'access_token' => $_SESSION['userIntegra']['access_token'],
+        'refresh_token' => $_SESSION['userIntegra']['refresh_token']
+    );
+    $dataJson = json_encode($data);
+    //echo $dataJson;
+    $ch = curl_init("10.5.16.109:63229/api/Token");
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $dataJson);
+
+    $server_output = curl_exec($ch);
+    //var_dump($server_output);
+    curl_close ($ch);
+
+    if($server_output) {
+        $userDisciplinas = json_decode($server_output, true);
+
+        foreach($userDisciplinas as $item) { //foreach element in $arr
+            $names[] = $item['nome']; 
+            $codigos[] = $item['codigo']; 
+            $salas[] = $item['sala']; 
+            $codigosTurma[] = $item['codigosTurma']; 
+            $ano[] = $item['ano']; 
+            $semestre[] = $item['semestre']; 
+            $turma[] = $item['turma']; 
+            $horarios[] = $item['horarios'];  //Cada item é um JSON
+        }
+
+        
+
+    } else {
+        echo"<script type='text/javascript'>
+        window.alert('Login e/ou senha incorretos.');
+        window.location.href='login.php';</script>";
+        die();
+    } */
+
 }
 
 ?>
@@ -24,6 +66,8 @@ if(!isset($_SESSION['user']))
             <p>Aqui estão as salas relacionadas com suas disciplinas deste período</p>
             <p>Clique sobre a disciplina para visualizar a(s) sala(s) relacionada(s)</p>
             <div id="accordion">
+           
+
                 <div class="card text-white bg-secondary">
                     <div class="card-header" id="headingOne">
                         <h5 class="mb-0">
@@ -60,7 +104,7 @@ if(!isset($_SESSION['user']))
                             </table>
                     </div>
                 </div>
-
+            
                 <div class="card text-white bg-secondary" style="padding: 0;">
                     <div class="card-header" id="headingOne2">
                         <h5 class="mb-0">
